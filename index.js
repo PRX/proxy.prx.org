9ea6b8d5ab602bcdd25917c54e9a5e3a6145cdaa
Chris Kalafarski <chris@farski.com>
2018-05-17 10:28:00 -0400

    Add Unbounce proxy rules
    Closes #17

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ const util = require('./lib/util');
 const HOSTS = (process.env.CANONICAL_HOSTS || 'www.prx.org,proxy.prx.org,proxy.staging.prx.tech,localhost:3000').split(',');
 
 const EXCHANGE_HOST = process.env.EXCHANGE_HOST || 'exchange.prx.org';
+const UNBOUNCE_HOST = process.env.UNBOUNCE_HOST || 'try.prx.org';
 const LISTEN_HOST = process.env.LISTEN_HOST || 'beta.prx.org';
 const HELP_HOST = process.env.HELP_HOST || 'help.prx.org';
 const CORPORATE_HOST = process.env.CORPORATE_HOST || 'corporate.prx.tech';
 const ROUTES = [
   [require('./routes/exchange-proxy'), new Proxy(EXCHANGE_HOST)],
+  [require('./routes/unbounce-proxy'), new Proxy(UNBOUNCE_HOST)],
   [require('./routes/listen-redirect'), new Redirect(LISTEN_HOST, require('./routes/listen-rewrite'))],
   [require('./routes/exchange-redirect'), new Redirect(EXCHANGE_HOST)],
   [require('./routes/help-redirect'), new Redirect(HELP_HOST, require('./routes/help-rewrite'))],
---
+++ routes/unbounce-proxy.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/**
+ * Paths proxied to Unbounce
+ */
+module.exports = [
+  /^\/prx-sponsorship(\/|$)/,
+  /^\/sponsorship(\/|$)/,
+  /^\/smithsonian-shows(\/|$)/,
+  /^\/inflection-point(\/|$)/,
+  /^\/que-pasa-midwest(\/|$)/,
+  /^\/out-of-the-blocks(\/|$)/,
+  /^\/versify(\/|$)/,
+  /^\/us-them(\/|$)/,
+  /^\/we-live-here(\/|$)/,
+  /^\/west-wing-weekly(\/|$)/,
+  /^\/ear-hustle(\/|$)/,
+  /^\/the-memory-palace(\/|$)/,
+  /^\/milk-street-radio(\/|$)/,
+  /^\/this-is-love(\/|$)/,
+  /^\/trump-con-law(\/|$)/,
+  /^\/theory-of-everything(\/|$)/,
+  /^\/kitchen-sisters(\/|$)/,
+  /^\/the-truth-v2(\/|$)/,
+  /^\/radio-diaries(\/|$)/,
+  /^\/loveradio(\/|$)/,
+  /^\/criminal-show(\/|$)/,
+  /^\/the-bugle(\/|$)/,
+  /^\/allusionist(\/|$)/,
+  /^\/mortified(\/|$)/,
+  /^\/song-exploder(\/|$)/,
+  /^\/99-invisible(\/|$)/,
+  /^\/radiotopia(\/|$)/,
+  /^\/esquire-classic(\/|$)/,
+  /^\/reveal(\/|$)/,
+  /^\/outside-magazine(\/|$)/,
+  /^\/htba(\/|$)/,
+  /^\/hermoney(\/|$)/,
+  /^\/orbital-path(\/|$)/,
+  /^\/fugitive-waves(\/|$)/,
+  /^\/podcast-garage-audiobook(\/|$)/,
+  /^\/podcast-garage-voiceover(\/|$)/,
+  /^\/podcast-garage(\/|$)/,
+  /^\/library-stories(\/|$)/
+];
+
+
+

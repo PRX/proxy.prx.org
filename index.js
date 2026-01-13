fce4908ec9b0e46bafb5b8a015406bf514077eab
Andrew Kuklewicz <andrew@beginsinwonder.com>
2026-01-12 19:14:12 -0500

    Add soundrise domain redirect handling

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const util = require('./lib/util');
 // These are the domains that this application is handling traffic for
 const HOSTS = (process.env.CANONICAL_HOSTS || 'www.prx.org,proxy.prx.org,proxy.staging.prx.tech,localhost:3000').split(',');
 const PRI_HOSTS = ['pri.org', 'www.pri.org', 'beta.pri.org', 'www-proxy-test.pri.org'];
+const SOUNDRISE_HOSTS = ['wearesoundrise.com', 'www.wearesoundrise.com'];
 
 // Values set using the x-prx-domain header
 // This is for cases where the proxy is running behind a CDN and is not getting
@@ -14,10 +15,12 @@ const PRI_HOSTS = ['pri.org', 'www.pri.org', 'beta.pri.org', 'www-proxy-test.pri
 // specific CDNs (i.e., different CDNs would handle different domains, like
 // one for prx.org and one for pri.org).
 const PRI_HEADER_DOMAIN = 'pri.org';
+const SOUNDRISE_HEADER_DOMAIN = 'wearesoundrise.com';
 
 // These are the domains that traffic is redirected to or used to fetch data
 // when being proxied. They are NOT domains for traffic being handled by this
 // application.
+const PRX_HOST = process.env.PRX_HOST || 'www.prx.org';
 const EXCHANGE_HOST = process.env.EXCHANGE_HOST || 'exchange.prx.org';
 const LISTEN_HOST = process.env.LISTEN_HOST || 'beta.prx.org';
 const HELP_HOST = process.env.HELP_HOST || 'help.prx.org';
@@ -28,6 +31,7 @@ const THEWORLD_EMBEDS_HOST = process.env.THEWORLD_EMBEDS_HOST || 'embedded.thewo
 const THEWORLD_FEEDS_HOST = process.env.THEWORLD_FEEDS_HOST || 'feeds.theworld.org';
 const THEWORLD_FILES_HOST = process.env.THEWORLD_FILES_HOST || 'files.theworld.org';
 const THEWORLD_SITEMAP_HOST = process.env.THEWORLD_SITEMAP_HOST || 'sitemap.theworld.org';
+const SOUNDRISE_HOST = process.env.SOUNDRISE_HOST || 'www.wearesoundrise.com';
 
 const PRX_ROUTES = [
   [require('./routes/exchange-proxy'), new Proxy(EXCHANGE_HOST)],
@@ -45,6 +49,8 @@ const PRI_ROUTES = [
   [require('./routes/pri-sitemap-redirect'), new Redirect(THEWORLD_SITEMAP_HOST, null, true, 301)],
   [[/./], new Redirect(THEWORLD_HOST, null, true, 301)],
 ];
+
+const SOUNDRISE_REDIRECT = new Redirect(PRX_HOST, (_p) => Promise.resolve('/sponsorship'));
 
 /**
  * Proxy requests here and there
@@ -66,6 +72,10 @@ exports.handler = async function handler(event) {
         return route = obj;
       }
     });
+  } else if (SOUNDRISE_HOSTS.includes(headers.host) || headers['x-prx-domain'] === SOUNDRISE_HEADER_DOMAIN) {
+    // Handle wearesoundrise.com traffic
+    domain = 'soundrise';
+    route = SOUNDRISE_REDIRECT;
   } else if (HOSTS.includes(headers.host)) {
     // Handle prx.org traffic
     domain = 'prx';
